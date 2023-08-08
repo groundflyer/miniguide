277b245f5a3dba9bb07625464b9e16acca32997c
Roman Saldygashev <sldg.roman@gmail.com>
2023-08-08 13:57:08 +0700

    added license headers

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,13 +1,24 @@
 // -*- C++ -*-
 // parser.cpp
+//
+//    Copyright (C) 2023  Roman Saldygashev
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "parser.hpp"
 
 #include <QDomDocument>
-#include <QMultiHash>
-#include <QSet>
-
-#include <QDebug>
 
 #include <utility>
 
